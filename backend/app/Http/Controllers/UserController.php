a42b1dd88c1485448857a1dcb1c6f8b7aaa7ea7f
Evgeni <ck.evgeni@gmail.com>
2018-01-28 02:14:15 +0200

    Init commit

---
+++ backend/app/Http/Controllers/UserController.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Http\Controllers;
+
+
+use App\User;
+use Illuminate\Http\Request;
+use Tymon\JWTAuth\Exceptions\JWTException;
+use Tymon\JWTAuth\JWTAuth;
+
+
+class UserController extends Controller
+{
+    /**
+     * @var \Tymon\JWTAuth\JWTAuth
+     */
+    protected $jwt;
+
+    public function __construct(JWTAuth $jwt)
+    {
+        $this->jwt = $jwt;
+    }
+
+    public function signup(Request $request) {
+        $this->validate($request, [
+            'name' => 'required',
+            'email' => 'required|email|unique:users',
+            'password' => 'required'
+        ]);
+
+        $user = new User([
+            'name' => $request->input('name'),
+            'email' => $request->input('email'),
+            'password' => password_hash($request->input('password'), PASSWORD_DEFAULT)
+        ]);
+        $user->save();
+
+        return response()->json([
+            'message' => 'Successfully created user!'
+        ], 201);
+    }
+
+    public function signin(Request $request) {
+        $this->validate($request, [
+            'name' => 'required',
+            'email' => 'required|email',
+            'password' => 'required'
+        ]);
+
+        try {
+            if(!$token =  $this->jwt->attempt($request->only('email', 'password'))) {
+                return response()->json([
+                    'error' => 'Invalid credentials!'
+            ], 401);
+
+            }
+        } catch (JWTException $e) {
+            return response()->json([
+                'error' => 'Could not create token!'
+            ], 500);
+        }
+
+        return response()->json([
+            'token' => $token
+        ]);
+    }
+}
